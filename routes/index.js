4cff5d02504a3ce91d63a09b61c6ff375e8c6e3d
Shawn Stern <stern.shawn@gmail.com>
2017-05-08 23:10:45 -0700

    Experimenting with basic express router functionality
    -Checking different objects such as req.query and req.params, and using methods such as res.send and res.json for formatting data to return to the user.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const shawn = { name: 'Shawn', age: 26, cool: true };
+  // res.json(shawn);
+  // res.send('Hey! It works!');
+  // res.send(req.query.age);
+  res.json(req.query);
 });
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
+})
 
 module.exports = router;
