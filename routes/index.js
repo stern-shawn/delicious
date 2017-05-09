ab93b47ae86f211d018fdef07eb1abc053d906e6
Shawn Stern <stern.shawn@gmail.com>
2017-05-09 11:31:53 -0700

    Experimenting with using PUG templating, passing data to template in express, and using javascript in PUG

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,17 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  const shawn = { name: 'Shawn', age: 26, cool: true };
+  // const shawn = { name: 'Shawn', age: 26, cool: true };
   // res.json(shawn);
   // res.send('Hey! It works!');
   // res.send(req.query.age);
-  res.json(req.query);
+  // res.json(req.query);
+  res.render('hello', Object.assign({
+    name: 'Shawn',
+    age: 26,
+    cool: true,
+    dog: req.query.dog,
+  }));
 });
 
 router.get('/reverse/:name', (req, res) => {
