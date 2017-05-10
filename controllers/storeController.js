a3a1f626bab026e142d521993c1f6af0029cf47f
Shawn Stern <stern.shawn@gmail.com>
2017-05-09 22:17:50 -0700

    Removing experimental middleware

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,12 +1,5 @@
-exports.myMiddleware = (req, res, next) => {
-  req.name = 'Shawn';
-  res.cookie('name', 'Shawn is cool', { maxAge: 9001 });
-  next();
-}
-
 exports.homePage = (req, res) => {
   // Render the index.pug template, and pass a title of 'Index'
-  console.log(req.name);
   res.render('index', {
     title: 'Index',
   })
