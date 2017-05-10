c2a452c09905956f5973d2dec72f9994b67f4fdd
Shawn Stern <stern.shawn@gmail.com>
2017-05-09 23:41:28 -0700

    Enabling creation of new stores by POSTing to /add
    -Using mongoose singleton to create a new Store document from the incoming reqest's body. Then we use async-await convention to save the new Store to the database and redirect to the home page once complete.
    -We need try/catch to handle any errors from async-await. Wrapping the createStore middleware in a helper function that will forward errors to the errorHandler middlewares. :)

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,14 +1,21 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   // Render the index.pug template, and pass a title of 'Index'
-  res.render('index', {
-    title: 'Index',
-  })
-}
+  res.render('index', { title: 'Index' });
+};
 
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
-}
+};
 
-exports.createStore = (req, res) => {
-  res.json(req.body);
-}
+// Time to try async-await!
+exports.createStore = async (req, res) => {
+  // req.body already defines the same keys as the schema, we can pass directly
+  const store = new Store(req.body);
+  // Actually save to MongoDB
+  await store.save();
+  // Redirect on completion, await makes this line not execute until store.save() is finished
+  res.redirect('/');
+};
