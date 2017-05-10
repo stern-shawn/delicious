5fe79f59caf63d8a68636ee95a49bdc4d52383e6
Shawn Stern <stern.shawn@gmail.com>
2017-05-09 21:39:55 -0700

    Factoring out route response code into a separate controller function

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  // const shawn = { name: 'Shawn', age: 26, cool: true };
-  // res.json(shawn);
-  // res.send('Hey! It works!');
-  // res.send(req.query.age);
-  // res.json(req.query);
-  res.render('hello', {
-    title: 'I Love Food'
-  });
-});
-
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-})
+router.get('/', storeController.homePage);
 
 module.exports = router;
