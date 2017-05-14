7a45c50129b0bcec9d1518f7450a79e7ec429490
Shawn Stern <stern.shawn@gmail.com>
2017-05-13 23:39:20 -0700

    Auto-generating unique slugs for Stores with the same name to avoid conflicts in DB

--- models/Store.js
+++ models/Store.js
@@ -37,15 +37,24 @@ const storeSchema = new mongoose.Schema({
 });
 
 // Automatically generate a slug for this entry before saving to DB
-storeSchema.pre('save', function autoGenSlug(next) {
+storeSchema.pre('save', async function autoGenSlug(next) {
   // Only generate a new slug if the name has been changed
   if (!this.isModified('name')) {
     next(); // Skip
     return; // Stop the function from running
   }
+
   this.slug = slug(this.name);
+  // Check for duplicate slugs. If we find any, generate a new slug of slug-1, slug-2, etc.
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  // this.constructor is how we reference Store while inside the schema
+  const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+
+  if (storesWithSlug) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+  }
+
   next();
-  // TODO make more robust by having unique slugs for same-named stores
 });
 
 module.exports = mongoose.model('Store', storeSchema);
