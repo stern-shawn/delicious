aa2633159865fe82198563999a576b43a6776596
Shawn Stern <stern.shawn@gmail.com>
2017-05-17 22:51:47 -0700

    Adding an extra layer of logic to search navigation to prevent errors

--- public/javascripts/modules/typeAhead.js
+++ public/javascripts/modules/typeAhead.js
@@ -50,31 +50,35 @@ function typeAhead(search) {
     const current = search.querySelector(`.${activeClass}`);
     // Get ALL possible nodes to we can decide which is next relative to current
     const items = search.querySelectorAll('.search__result');
-    let next;
-    if (e.keyCode === 40 && current) {
-      // If downarrow and there is no next element, fallback to the first element
-      next = current.nextElementSibling || items[0];
-    } else if (e.keyCode === 40) {
-      // If it's the first time hitting down, select the first element
-      next = items[0];
-    } else if (e.keyCode === 38 && current) {
-      // Conversely for uparrow while something is selected, go to prev or last as a fallback
-      next = current.previousElementSibling || items[items.length - 1];
-    } else if (e.keyCode === 38) {
-      // If it's the first time hitting up, select the last element
-      next = items[items.length - 1];
-    } else if (e.keyCode === 13 && current.href) {
-      // If it's enter... navigate there
-      window.location = current.href;
-      // If we do navigate elsewhere, don't try to edit the menu styling as below (it'll be gone)
-      return;
-    }
+    // Only apply the selection logic if there are results in the first place...
+    // Otherwise you'll get errors for trying to modify classList of undefined, etc.
+    if (items.length) {
+      let next;
+      if (e.keyCode === 40 && current) {
+        // If downarrow and there is no next element, fallback to the first element
+        next = current.nextElementSibling || items[0];
+      } else if (e.keyCode === 40) {
+        // If it's the first time hitting down, select the first element
+        next = items[0];
+      } else if (e.keyCode === 38 && current) {
+        // Conversely for uparrow while something is selected, go to prev or last as a fallback
+        next = current.previousElementSibling || items[items.length - 1];
+      } else if (e.keyCode === 38) {
+        // If it's the first time hitting up, select the last element
+        next = items[items.length - 1];
+      } else if (e.keyCode === 13 && current.href) {
+        // If it's enter... navigate there
+        window.location = current.href;
+        // If we do navigate elsewhere, don't try to edit the menu styling as below (it'll be gone)
+        return;
+      }
 
-    // Now that we have next selected, clear active class then add to this element
-    if (current) {
-      current.classList.remove(activeClass);
+      // Now that we have next selected, clear active class then add to this element
+      if (current) {
+        current.classList.remove(activeClass);
+      }
+      next.classList.add(activeClass);
     }
-    next.classList.add(activeClass);
   });
 }
 
