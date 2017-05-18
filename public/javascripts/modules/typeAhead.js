53f9a4b2687539fe12cd57b05edca8237efc4dde
Shawn Stern <stern.shawn@gmail.com>
2017-05-17 22:49:45 -0700

    Adding interactive selection and navigation to search results dropdown

--- public/javascripts/modules/typeAhead.js
+++ public/javascripts/modules/typeAhead.js
@@ -39,6 +39,43 @@ function typeAhead(search) {
         console.error(err);
       });
   });
+
+  // Handle keyboard interaction
+  searchInput.on('keyup', (e) => {
+    // If keyCode isn't enter, up, or down, do nothing
+    if (![13, 38, 40].includes(e.keyCode)) return;
+
+    const activeClass = 'search__result--active';
+    // Get the currently active node
+    const current = search.querySelector(`.${activeClass}`);
+    // Get ALL possible nodes to we can decide which is next relative to current
+    const items = search.querySelectorAll('.search__result');
+    let next;
+    if (e.keyCode === 40 && current) {
+      // If downarrow and there is no next element, fallback to the first element
+      next = current.nextElementSibling || items[0];
+    } else if (e.keyCode === 40) {
+      // If it's the first time hitting down, select the first element
+      next = items[0];
+    } else if (e.keyCode === 38 && current) {
+      // Conversely for uparrow while something is selected, go to prev or last as a fallback
+      next = current.previousElementSibling || items[items.length - 1];
+    } else if (e.keyCode === 38) {
+      // If it's the first time hitting up, select the last element
+      next = items[items.length - 1];
+    } else if (e.keyCode === 13 && current.href) {
+      // If it's enter... navigate there
+      window.location = current.href;
+      // If we do navigate elsewhere, don't try to edit the menu styling as below (it'll be gone)
+      return;
+    }
+
+    // Now that we have next selected, clear active class then add to this element
+    if (current) {
+      current.classList.remove(activeClass);
+    }
+    next.classList.add(activeClass);
+  });
 }
 
 export default typeAhead;
