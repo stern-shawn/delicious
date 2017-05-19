7c458565816ad9e9ff110bc9b49423e5a6abfd90
Shawn Stern <stern.shawn@gmail.com>
2017-05-18 22:39:42 -0700

    Adding bounds and map centering to better display result markers

--- public/javascripts/modules/map.js
+++ public/javascripts/modules/map.js
@@ -10,18 +10,28 @@ function loadPlaces(map, lat = 43.2, lng = -79.8) {
   axios.get(`/api/stores/near?lat=${lat}&lng=${lng}`)
     .then((res) => {
       const places = res.data;
+
       if (!places.length) {
         alert('no places found');
         return;
       }
 
+      // Create map bounds
+      const bounds = new google.maps.LatLngBounds();
+
+      // Create our list of markers to render to the map
       const markers = places.map((place) => {
         const [placeLng, placeLat] = place.location.coordinates;
         const position = { lat: placeLat, lng: placeLng };
+        bounds.extend(position);
         const marker = new google.maps.Marker({ map, position });
         marker.place = place;
         return marker;
       });
+
+      // Center and zoom map to fit our markers
+      map.setCenter(bounds.getCenter());
+      map.fitBounds(bounds);
     });
 }
 
@@ -34,8 +44,6 @@ function makeMap(mapDiv) {
 
   const input = $('[name="geolocate"]');
   const autocomplete = new google.maps.places.Autocomplete(input);
-
-
 }
 
 export default makeMap;
